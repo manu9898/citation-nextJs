7a301d57ec6e746c888e6c148d3f377c116edd22
manu9898 <manumuscat3@gmail.com>
2025-04-16 12:35:30 +0200

    ajout des fonctionnalités supression et update, alignement horizontal des citations et des deux présentes fonctionnalités

---
+++ src/app/citations/[citationId]/page.tsx
@@ -0,0 +1,31 @@
+import {prisma} from "@/lib/prisma";
+import {Card, CardHeader, CardTitle} from "@/components/ui/card";
+
+
+export default async function Page(props:{
+    params:Promise<{
+        citationId:string;
+    }>;
+    searchParams: Promise<Record<string, string | string[]>>
+}) {
+    const params = await props.params;
+    const citationId = params.citationId;
+    const citation = await prisma.citation.findFirst({
+        where: {
+            id: Number(params.citationId)
+        }
+    })
+
+    if (!citation) {
+        return <Card>
+            <CardHeader></CardHeader>
+            <CardTitle>The citation {citationId} not exist</CardTitle>
+        </Card>
+    }
+return <Card className="p-4 flex items-start gap-4" key={citation.id}>
+    <div className="flex flex-col gap-2 flex-1">
+        <p>{citation.text}</p>
+        <p>-- {citation.author}</p>
+    </div>
+</Card>
+}
